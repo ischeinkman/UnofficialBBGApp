7483c2a5764784c3d95bc6ca5bcd2216ba85811d
ischeinkman <scheinkman.ilan@gmail.com>
2016-01-07 11:56:32 -0800

    Changed to BBG-based constants

--- app/src/main/java/org/ramonaza/unofficialazaapp/colorbook/backend/ColorbookConstants.java
+++ app/src/main/java/org/ramonaza/unofficialazaapp/colorbook/backend/ColorbookConstants.java
@@ -9,9 +9,9 @@ public final class ColorbookConstants {
 
 
     public static final int BEGINNING = 0;
-    public static final int HISTORY_AND_STRUCTURE = 12;
-    public static final int PROGRAMMING = 36;
-    public static final int RITUALS_AND_PROCEDURES = 54;
+    public static final int HISTORY_AND_STRUCTURE = 14;
+    public static final int PROGRAMMING = 38;
+    public static final int RITUALS_AND_PROCEDURES = 56;
     public static final int OTHER = 68;
     public static final int INIT_DISPLACEMENT = 1;
 
